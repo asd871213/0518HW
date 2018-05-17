a5015348f096b03d3e6203b9504820d0d38de72c
peihan <asd871213@gmail.com>
2018-05-17 18:44:25 +0800

    main.c

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	
-int EnglishGrade,MathGrade,Sum;
-	printf("enter EnglishGrade=>");
-	scanf("%d",&EnglishGrade);
-	printf("enter MathGrade=>");
-	scanf("%d",&MathGrade);
+int EnglishGrade,MathGrade,Sum; /*宣告變數:英文成績.數學成績和總和*/
+	printf("enter EnglishGrade=>"); /*印出提示字元要求使用者輸入英文成績*/
+	scanf("%d",&EnglishGrade);/*輸入英文成績*/
+	printf("enter MathGrade=>");/*印出提示字元要求使用者輸入數學成績*/
+	scanf("%d",&MathGrade);/*輸入數學成績*/
 
-	Sum=EnglishGrade+MathGrade;
-	printf("Sum=%d\n",Sum);
+	Sum=EnglishGrade+MathGrade;/*加總*/
+	printf("Sum=%d\n",Sum);/*印出加總後成績*/
 	
 return 0;
 }
